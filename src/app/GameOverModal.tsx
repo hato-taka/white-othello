19a52c0cb5fdcda68074e619bc7cad046e4af867
hato-taka <n28hashimoto@gmail.com>
2025-04-06 18:37:12 +0900

    GameOverModalに賞金リンクを追加し、勝者表示を改善。Othelloコンポーネントのスタイルを調整。

--- src/app/GameOverModal.tsx
+++ src/app/GameOverModal.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface GameOverModalProps {
@@ -7,17 +8,31 @@ interface GameOverModalProps {
   onRestart: () => void;
 }
 
-const GameOverModal: React.FC<GameOverModalProps> = ({ winner, white, black, onRestart }) => {
+const GameOverModal: React.FC<GameOverModalProps> = ({
+  winner,
+  white,
+  black,
+  onRestart,
+}) => {
+  // TODO: 46円の賞金のルーレット画面を作る
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-80 flex items-center justify-center z-[9999]">
       <div className="bg-white text-black p-10 rounded-2xl text-center">
         <p className="text-xl mb-1">Winner: {winner}</p>
         {winner === "プレイヤー" && (
-          <p className="text-2xl font-bold text-green-600 mt-2">
-            おめでとうございます！
-          </p>
+          <>
+            <p className="text-2xl font-bold text-green-600 my-2">
+              おめでとうございます！
+            </p>
+            <Link
+              href="https://pay.paypay.ne.jp/uqdw9JMfSqz9Dmw3"
+              className="font-bold text-blue-600 text-2xl underline hover:text-blue-800"
+            >
+              賞金ゲット！
+            </Link>
+          </>
         )}
-        <p className="text-lg mb-2">Final Score</p>
+        <p className="text-lg my-2">Final Score</p>
         <p className="mb-4">
           ⚪ プレイヤー: {white} | ⚫ CPU: {black}
         </p>
--- src/app/Othello.tsx
+++ src/app/Othello.tsx
@@ -183,6 +183,8 @@ const Othello: React.FC = () => {
         />
       )}
 
+
+
       <style>
         {`
           @keyframes flip {
